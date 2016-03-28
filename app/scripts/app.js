445795b86515d6b2cd6b069a752f021a76a5bb55
Triamer <dexterquin@gmail.com>
2016-03-29 00:08:10 +0300

    cleaned up app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,32 +5,9 @@ angular.module('scheduler', ['sdGridModule'])
     function($scope, $http) {
         var zoomScale = [0.25, 0.5, 1, 2, 4, 8];
 
-        $scope.isSamples1 = true;
-        $scope.$watch('isSamples1', function() {
-            var samplesFile = ($scope.isSamples1) ? 'api/entities1.json' : 'api/entities2.json';
-
-            $http.get(samplesFile).
-                success(function(response) {
-                    $scope.sampleData = response;
-            });
-        });
-
-        $scope.isRangeChanged = false;
-        $scope.$watch('isRangeChanged', function() {
-            var ranges = ($scope.isRangeChanged) ?
-                [
-                    "-8:00-5:30",
-                    "-15:00-2:30"
-                ] :
-                [
-                    "-11:00-5:30",
-                    "-17:00-2:30"
-                ];
-
-            if ($scope.sampleData) {
-                $scope.sampleData[0].schedules[0].ranges = ranges;
-            }
-        });
+        $http.get('api/entities1.json').then(function(response) {
+            $scope.sampleData = response.data;
+        })
 
         $scope.increaseZoom = function() {
             $scope.hoursPerCell = zoomScale[Math.min(zoomScale.length - 1, zoomScale.indexOf($scope.hoursPerCell) + 1)];
@@ -41,7 +18,7 @@ angular.module('scheduler', ['sdGridModule'])
         };
 
         $scope.dates = {
-            from: new Date(2016, 2, 28),
+            from: new Date(2016, 2, 29),
             to: new Date(2016, 2, 30)
         };
 
